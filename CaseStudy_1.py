4e9edd48e7c088f47e67369fcbe4d8b77eda53a4
Sutanu <sutanukhasnobisece@gmail.com>
2020-09-21 19:57:36 +0000

    Commiting from newly created branch

--- CaseStudy_1.py
+++ CaseStudy_1.py
@@ -3,4 +3,4 @@ lastName)
 print("Your name is: ")
 print(firstName + lastName)
 endprocedure
-showNames("Sutanu", "Khasnobis")
+showNames("Sutanu", "Khasnobis") //Adding comment line in new branch. This is my name.
